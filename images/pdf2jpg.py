51dbad6e37b2b32b009c5ded2f09c1df644a40ea
Roland Fehr <62805035+rfehr-creator@users.noreply.github.com>
2024-05-31 11:04:18 -0500

    resize images before predicting

--- images/pdf2jpg.py
+++ images/pdf2jpg.py
@@ -1,7 +1,7 @@
 import urllib
-
 import numpy as np
 from pdf2image import convert_from_path, convert_from_bytes
+
 
 # url pdf to jpeg
 def pdf2JpgFromURL(url: str):
@@ -9,16 +9,16 @@ def pdf2JpgFromURL(url: str):
     with urllib.request.urlopen(url) as response:
         # Read the PDF content
         pdf_content = response.read()
-        
+
         # convert to jpeg
         images = convert_from_bytes(pdf_content)
-        
-        print(len(images))
-        # return as numpy array
+
+        print("pdf2JpgFromURL images len: {}".format(len(images)))
         return [np.array(image) for image in images]
+
 
 # file pdf to jpeg
 def pdf2Jpg(pdf_path):
     images = convert_from_path(pdf_path)
-    
+
     return [np.array(image) for image in images]
